92f53fde05c089a4951ed64ba78de6d568e061b1
rvishakhs <rvishakhs@gmail.com>
2025-03-26 12:08:09 +0000

    writing prompt template

--- src/prompt.py
+++ src/prompt.py
@@ -3,4 +3,22 @@ def prepare_prompt(resume_txt, jd_txt):
     if not resume_txt or not jd_txt:
         raise ValueError("Resume and JD text cannot be empty")
     
-    prompt_template = " "
+    prompt_template = """
+    Act as an expert ATS (Applicant Tracking System) and match the resume with the job description, specialist with deep expertise in:
+    - Technical fields
+    - Sofetware engineering 
+    - Data science
+    - Machine learning
+    - Data analysis
+    - web development
+    - full stack development
+
+    Evaluate the following resume against the job description. COnsider that the job market is 
+    highly competitive and the resume should be tailored to the job description. 
+
+    Resume:
+    {resume_txt}
+
+    Job Description:
+    {jd_txt}
+    """
